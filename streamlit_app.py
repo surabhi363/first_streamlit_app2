ba9903dc106959ff64e095b122dc23a6b2ac59f1
Surabhi Pandey <120398877+surabhi363@users.noreply.github.com>
2023-01-19 23:36:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
+CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+
 import streamlit
 import streamlit
 import snowflake.connector
@@ -33,5 +42,3 @@ caption= product_caption
 streamlit.write('Price: ', df2[1])
 streamlit.write('Sizes Available: ',df2[2])
 streamlit.write(df2[3])
-THIS IS AN OPTIONAL EX
--
